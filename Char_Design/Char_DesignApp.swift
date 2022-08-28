46dec24b47d994a02c6f9dc20aa5470fe792ff36
hazem-smawy <hazemsmawy@gmail.com>
2022-08-29 00:39:40 +0300

    Initial Commit

---
+++ Char_Design/Char_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Char_DesignApp.swift
+//  Char_Design
+//
+//  Created by hazem smawy on 02/02/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Char_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Char_Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Char_Design
+//
+//  Created by hazem smawy on 02/02/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
